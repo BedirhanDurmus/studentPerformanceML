daa42dceed128acc9df7a7313441729dc4707fff
BedirhanDurmus <bedirhan_durmus@hotmail.com>
2025-02-24 06:15:27 +0300

    Data Transformation

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,10 +1,11 @@
+import os
 import sys
 from dataclasses import dataclass
 import pandas as pd
 import numpy as np
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler,OneHotEncoder
 from sklearn.pipeline import Pipeline
 from src.exception import CustomException
 from src.logger import logging
